ae57e83e9c574d760e9e8fddd84606a40842a4a5
Trung Khiêm <trungkhiem1412@gmail.com>
2022-07-12 16:59:48 +0700

    update main clone

--- mvc/views/body/footer.php
+++ mvc/views/body/footer.php
@@ -57,7 +57,7 @@
                 </span>
                 <span>
                     401 BROADWAY, 24TH FLOOR
-                    ORCHARD VIEW, LONDON, UK, khiem dep trai
+                    ORCHARD VIEW, LONDON, UK
                 </span>
             </div>
         </div>
