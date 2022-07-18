1caf4cac7f4883d1a49cf71d890d20586579d33d
Tien Bach <phantienbachf8@gmail.com>
2022-07-18 08:57:51 +0700

    fix admincss, fix cart

--- mvc/views/cart.php
+++ mvc/views/cart.php
@@ -54,6 +54,45 @@
                                                         <label for="quantity">Số lượng:</label>
                                                         <input id="quantity" type="number" value="1"
                                                             class="form-control quantity-input">
+                                                            <a href="#" target="_self"><button type="button"
+                                            class="delete mt-2 btn btn-danger">
+                                            Xóa</button></a>
+                                                    </div>
+                                                    <div class="col-md-3 price">
+                                                        <span>5.000.000.000 VND</span>
+                                                    </div>
+                                                 
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <img class="img-fluid mx-auto d-block image"
+                                                src="public/images/den-bi-xenon4.jpg">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="info">
+                                                <div class="row">
+                                                    <div class="col-md-5 product-name">
+                                                        <div class="product-name">
+                                                            <a href="#">Isuzu D max 2022 hilander neww</a>
+                                                            <div class="product-info">
+                                                                <div>Hãng :<span class="value">Isuzu</span></div>
+                                                                <div>Số :<span class="value">Tự động</span></div>
+                                                                <div>Sản xuất :<span class="value">2022</span></div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4 quantity">
+                                                        <label for="quantity">Số lượng:</label>
+                                                        <input id="quantity" type="number" value="1"
+                                                            class="form-control quantity-input">
+                                                            <a href="#" target="_self"><button type="button"
+                                            class="delete mt-2 btn btn-danger">
+                                            Xóa</button></a>
                                                     </div>
                                                     <div class="col-md-3 price">
                                                         <span>5.000.000.000 VND</span>
@@ -86,38 +125,9 @@
                                                         <label for="quantity">Số lượng:</label>
                                                         <input id="quantity" type="number" value="1"
                                                             class="form-control quantity-input">
-                                                    </div>
-                                                    <div class="col-md-3 price">
-                                                        <span>5.000.000.000 VND</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="product">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <img class="img-fluid mx-auto d-block image"
-                                                src="public/images/den-bi-xenon4.jpg">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="info">
-                                                <div class="row">
-                                                    <div class="col-md-5 product-name">
-                                                        <div class="product-name">
-                                                            <a href="#">Isuzu D max 2022 hilander neww</a>
-                                                            <div class="product-info">
-                                                                <div>Hãng :<span class="value">Isuzu</span></div>
-                                                                <div>Số :<span class="value">Tự động</span></div>
-                                                                <div>Sản xuất :<span class="value">2022</span></div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4 quantity">
-                                                        <label for="quantity">Số lượng:</label>
-                                                        <input id="quantity" type="number" value="1"
-                                                            class="form-control quantity-input">
+                                                            <a href="#" target="_self"><button type="button"
+                                            class="delete mt-2 btn btn-danger">
+                                            Xóa</button></a>    
                                                     </div>
                                                     <div class="col-md-3 price">
                                                         <span>5.000.000.000 VND</span>
