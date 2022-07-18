264fe26f19ddb4d86f31bc4287da9f8d54fc2eee
NQD <duynqps19550@fpt.edu.vn>
2022-07-18 22:08:05 +0700

    update add san pham

--- mvc/views/body/header.php
+++ mvc/views/body/header.php
@@ -16,7 +16,9 @@
     <!-- Navbar PC -->
     <nav class="navbar navbar-expand-md bg-light">
         <div class="container-fluid position-relative">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!-- test -->
@@ -26,7 +28,8 @@
                         <a class="nav-link active" aria-current="page" href="home" target="_self">Trang Chủ</a>
                     </li>
                     <li class="nav-item dropdown-center dropnav-a">
-                        <a class="nav-link" href="#" tabindex="0" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                        <a class="nav-link" href="#" tabindex="0" data-bs-toggle="dropdown" aria-expanded="false"
+                            data-bs-auto-close="outside">
                             Sản phẩm <i class="bi bi-caret-down-fill"></i>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-light">
@@ -58,9 +61,9 @@
                     </li>
                     <!-- user -->
                     <li class="nav-item d-none d-md-block dropstart dropstart-a">
-                        <a href="#" tabindex="0" class="nav-link fs-4" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                        <a href="#" tabindex="0" class="nav-link fs-4" data-bs-toggle="dropdown" aria-expanded="false"
+                            data-bs-auto-close="outside">
                             <?php
-<<<<<<< HEAD
                                 if(isset($_SESSION['nameUS'])){
                                     ?>
                             <i class="bi bi-person-fill"></i><span
@@ -84,64 +87,38 @@
                                                         xuất</a>
                                                 </li>
                                             </ul>
-=======
-                            if (isset($_SESSION['nameUS'])) {
-                            ?>
-                                <i class="bi bi-person-fill"></i><?= $_SESSION['nameUS'] ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+
+                                }else{ ?>
+                                <i class="bi bi-person-fill"></i>
                                 <div class="dropdown-menu dropdown-menu-start p-3">
                                     <div class="drop-account">
                                         <div class="avt-account">
-                                            <img src="<?= $_SESSION['urlUS'] ?>" alt="">
+                                            <img src="public/images/725x560-1.webp" alt="">
                                         </div>
                                         <div class="name-account">
-                                            <a href="admin"><?= $_SESSION['nameUS'] ?></a>
+                                            <a href="admin"></a>
                                         </div>
                                         <div class="drop-account-btn">
                                             <div class="btn-box">
                                                 <ul class="d-flex p-0">
                                                     <li class="p-1">
-                                                        <a class="btn" href="admin" target="_self">Tài Khoản</a>
+                                                        <a class="btn" href="login" target="_self">Đăng nhập</a>
                                                     </li>
                                                     <li class="p-1">
-                                                        <a class="btn" href="login/dangxuat" target="_self">Đăng xuất</a>
+                                                        <a class="btn" href="login" target="_self">Đăng ký</a>
                                                     </li>
                                                 </ul>
                                             </div>
->>>>>>> dce88c466ede29eef4f0af8ebbce2b0de8b81f33
                                         </div>
                                     </div>
                                 </div>
-                            <?php
-<<<<<<< HEAD
-                                }else{ ?>
-                            <i class="bi bi-person-fill"></i>
-                            <div class="dropdown-menu dropdown-menu-start p-3">
-                                <div class="drop-account">
-                                    <div class="avt-account">
-                                        <img src="public/images/725x560-1.webp" alt="">
-                                    </div>
-                                    <div class="name-account">
-                                        <a href="admin"></a>
-                                    </div>
-                                    <div class="drop-account-btn">
-                                        <div class="btn-box">
-                                            <ul class="d-flex p-0">
-                                                <li class="p-1">
-                                                    <a class="btn" href="login" target="_self">Đăng nhập</a>
-                                                </li>
-                                                <li class="p-1">
-                                                    <a class="btn" href="login" target="_self">Đăng ký</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-=======
-                            } else { ?>
-                                <a href="login" target="_self"><i class="bi bi-person-fill"></i></a>
->>>>>>> dce88c466ede29eef4f0af8ebbce2b0de8b81f33
-                            <?php }
+                                <!-- } else { ?> -->
+                                <!-- <a href="login" target="_self"><i class="bi bi-person-fill"></i></a> -->
+                                <?php }
                             ?>
 
                         </a>
@@ -150,7 +127,9 @@
                     <li class="nav-item">
                         <a tabindex="0" class="nav-link fs-4" href="cart" target="_self">
                             <i class="bi bi-cart position-relative">
-                                <span class="position-absolute bottom-0 start-100 translate-middle badge rounded-pill bg-black fw-normal" style="font-size: 10px;">
+                                <span
+                                    class="position-absolute bottom-0 start-100 translate-middle badge rounded-pill bg-black fw-normal"
+                                    style="font-size: 10px;">
                                     1
                                 </span>
                             </i>
@@ -169,7 +148,8 @@
                     </li> -->
                     <li class="nav-item">
                         <!-- <a class="nav-link" href="product" target="_self">Sản Phẩm</a> -->
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             Sản Phẩm
                         </a>
                         <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkDropdownMenuLink">
@@ -191,7 +171,7 @@
                                 <i class="bi bi-person-fill"></i> Đăng Nhập
                             </a>
                         </div>
-                        |
+
                         <div class="box-2">
                             <a class="nav-link" href="login" target="_self">
                                 <i class="bi bi-shield-lock-fill"></i> Đăng Ký
--- public/js/admin.js
+++ public/js/admin.js
@@ -17,3 +17,103 @@ function loadFunction() {
 }
 document.getElementById("today").innerHTML =
   '<input type="text" name="dateTao" value="' + loadFunction() + '" ><br>';
+
+function preimg() {
+  var inputIMG = document.querySelector(".imgPre").files;
+  console.log(inputIMG.length);
+  if (inputIMG.length > 0) {
+    var fileToLoad = inputIMG[0];
+    console.log(fileToLoad);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var src = event.target.result;
+      var newIMG = document.createElement("img");
+      newIMG.src = src;
+      newIMG.style.width = "200px";
+      newIMG.style.height = "100px";
+      console.log(src);
+      document.querySelector(".img_upp").innerHTML = newIMG.outerHTML;
+    };
+    reader.readAsDataURL(fileToLoad);
+  }
+}
+
+function preimg1() {
+  var inputIMG = document.querySelector(".imgPre1").files;
+  console.log(inputIMG.length);
+  if (inputIMG.length > 0) {
+    var fileToLoad = inputIMG[0];
+    console.log(fileToLoad);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var src = event.target.result;
+      var newIMG = document.createElement("img");
+      newIMG.src = src;
+      newIMG.style.width = "200px";
+      newIMG.style.height = "100px";
+      console.log(src);
+      document.querySelector(".img_upp1").innerHTML = newIMG.outerHTML;
+    };
+    reader.readAsDataURL(fileToLoad);
+  }
+}
+
+function preimg2() {
+  var inputIMG = document.querySelector(".imgPre2").files;
+  console.log(inputIMG.length);
+  if (inputIMG.length > 0) {
+    var fileToLoad = inputIMG[0];
+    console.log(fileToLoad);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var src = event.target.result;
+      var newIMG = document.createElement("img");
+      newIMG.src = src;
+      newIMG.style.width = "200px";
+      newIMG.style.height = "100px";
+      console.log(src);
+      document.querySelector(".img_upp2").innerHTML = newIMG.outerHTML;
+    };
+    reader.readAsDataURL(fileToLoad);
+  }
+}
+
+function preimg3() {
+  var inputIMG = document.querySelector(".imgPre3").files;
+  console.log(inputIMG.length);
+  if (inputIMG.length > 0) {
+    var fileToLoad = inputIMG[0];
+    console.log(fileToLoad);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var src = event.target.result;
+      var newIMG = document.createElement("img");
+      newIMG.src = src;
+      newIMG.style.width = "200px";
+      newIMG.style.height = "100px";
+      console.log(src);
+      document.querySelector(".img_upp3").innerHTML = newIMG.outerHTML;
+    };
+    reader.readAsDataURL(fileToLoad);
+  }
+}
+
+function preimg4() {
+  var inputIMG = document.querySelector(".imgPre4").files;
+  console.log(inputIMG.length);
+  if (inputIMG.length > 0) {
+    var fileToLoad = inputIMG[0];
+    console.log(fileToLoad);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var src = event.target.result;
+      var newIMG = document.createElement("img");
+      newIMG.src = src;
+      newIMG.style.width = "200px";
+      newIMG.style.height = "100px";
+      console.log(src);
+      document.querySelector(".img_upp4").innerHTML = newIMG.outerHTML;
+    };
+    reader.readAsDataURL(fileToLoad);
+  }
+}
