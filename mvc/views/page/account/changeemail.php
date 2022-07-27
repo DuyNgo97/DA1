6a9ea18007ad8696bc6811247c0a048a7d731205
Tien Bach <phantienbachf8@gmail.com>
2022-07-27 08:53:19 +0700

    up

--- mvc/views/page/account/changeemail.php
+++ mvc/views/page/account/changeemail.php
@@ -12,13 +12,13 @@ if (isset($data["arrEd"])) {
 </h1>
 <form action="account/UpdateEmail" target="account.php" method="post">
     <label for="psw"><b>Email</b></label>
-    <input type="email" name="email" value="">
+    <input class="form-control w-25 mb-2 type=" email" name="email" value="">
 
     <?php if (isset($data["check"])) {
         echo '<h1> update thanh cong <h1>';
     } ?>
 
     <div class="clearfix">
-        <button type="submit" name="submit" class="submit">Sửa</button>
+        <button type="submit" name="submit" class="submit btn btn-danger">Sửa</button>
     </div>
 </form>
--- mvc/views/page/account/changesdt.php
+++ mvc/views/page/account/changesdt.php
@@ -12,13 +12,13 @@ if (isset($data["arrEd"])) {
 </h1>
 <form action="account/UpdateSDT" target="account.php" method="post">
     <label for="psw"><b>Số điện thoại</b></label>
-    <input type="number" name="sdt" id="">
+    <input class="form-control w-25 mb-2 type=" number" name="sdt" id="">
 
     <?php if (isset($data["check"])) {
         echo '<h1> update thanh cong <h1>';
     } ?>
 
     <div class="clearfix">
-        <button type="submit" name="submit" class="submit">Sửa</button>
+        <button type="submit" name="submit" class="submit btn btn-danger">Sửa</button>
     </div>
 </form>
